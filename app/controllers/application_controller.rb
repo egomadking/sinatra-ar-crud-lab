29001c6eb5097b67b27faaf8698e47cf1daff646
Roy Mosby <ego.madking@gmail.com>
2021-01-29 12:31:39 -0500

    commit tree ca7fe0e34202cb02a008d24c2f8f262d91ff22a7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,47 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    something = <<-HTML
+      <ul>
+        <li><a href="/articles">Go to articles</a></li>
+        <li><a href="/articles/new">Create new entry</a></li>
+      <ul>
+    HTML
   end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+  
+  get '/articles/new' do
+    erb :create
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  post "/articles" do
+    @article = Article.create(title: params[:title], content: params[:content])
+    redirect to "/articles/#{@article.id}"
+  end
+
+  patch "/articles/:id" do
+    @article = Article.find(params[:id])
+    @article.update(title: params[:title], content: params[:content])
+    redirect to "/articles/#{@article.id}"
+  end
+
+  delete "/articles/:id" do
+    @article = Article.find(params[:id])
+    @article.destroy
+  end
+
 end
